c0a491c67004b10071f67acbd1a1ef37ff490768
Lucas Gabrielson <>
2021-03-03 10:35:29 -0600

    completed styling with gabin

--- client.js
+++ client.js
@@ -1,42 +1,40 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  $('#btn').on('click', renderDom)
-
-};// end handleReady
-
+  $("#btn").on("click", renderDom);
+} // end handleReady
 
 const employees = [
   {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
-    reviewRating: 3
+    name: "Atticus",
+    employeeNumber: "2405",
+    annualSalary: "47000",
+    reviewRating: 3,
   },
   {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
-    reviewRating: 4
+    name: "Jem",
+    employeeNumber: "62347",
+    annualSalary: "63500",
+    reviewRating: 4,
   },
   {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
-    reviewRating: 5
+    name: "Scout",
+    employeeNumber: "6243",
+    annualSalary: "74750",
+    reviewRating: 5,
   },
   {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
-    reviewRating: 1
+    name: "Robert",
+    employeeNumber: "26835",
+    annualSalary: "66000",
+    reviewRating: 1,
   },
   {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
-    reviewRating: 1
-  }
+    name: "Mayella",
+    employeeNumber: "89068",
+    annualSalary: "35000",
+    reviewRating: 1,
+  },
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -48,59 +46,65 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
 
 // create new employee bonus array
 let employeesBonusArr = [];
 
 // create new employee bonus objects and push them into the employeeBonus array
 function bonusCalculator(employee) {
-    let bonusPercentage = 0;
-    if( Number(employee.annualSalary) > 65000 ) {
-      bonusPercentage -= 1;
-    }
-    if( employee.employeeNumber.length > 3 ) {
-      bonusPercentage += 5;
-    }
-    if( employee.reviewRating === 2 ) {
-      bonusPercentage = 0;
-    }
-    else if( employee.reviewRating === 3 ) {
-      bonusPercentage += 4;
-    }
-    else if( employee.reviewRating === 4 ) {
-      bonusPercentage += 6;
-    }
-    else if( employee.reviewRating === 5 ) {
-      bonusPercentage += 10;
-    }
+  let bonusPercentage = 0;
+  if (Number(employee.annualSalary) > 65000) {
+    bonusPercentage -= 1;
+  }
+  if (employee.employeeNumber.length > 3) {
+    bonusPercentage += 5;
+  }
+  if (employee.reviewRating === 2) {
+    bonusPercentage = 0;
+  } else if (employee.reviewRating === 3) {
+    bonusPercentage += 4;
+  } else if (employee.reviewRating === 4) {
+    bonusPercentage += 6;
+  } else if (employee.reviewRating === 5) {
+    bonusPercentage += 10;
+  }
 
-    if(bonusPercentage >= 13) {
-      bonusPercentage = 13;
-    }
-    else if(bonusPercentage < 0) {
-      bonusPercentage = 0;
-    }
+  if(bonusPercentage >= 13) {
+    bonusPercentage = 13;
+  }
+  else if(bonusPercentage < 0) {
+    bonusPercentage = 0;
+  }
 
-    let totalBonus = Math.round(Number(employee.annualSalary) * bonusPercentage / 100);
-    let totalCompensation = totalBonus + Number(employee.annualSalary);
+  let totalBonus = Math.round(
+    (Number(employee.annualSalary) * bonusPercentage) / 100
+  );
+  let totalCompensation = totalBonus + Number(employee.annualSalary);
 
-    let employeeBonus = {
-      name: employee.name,
-      bonusPercentage: bonusPercentage,
-      totalCompensation: totalCompensation,
-      totalBonus: totalBonus
-    }
+  let employeeBonus = {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: totalCompensation,
+    totalBonus: totalBonus,
+  };
   return employeeBonus;
 } // end bonusCalculator
 
 function renderDom() {
-  let el = $( '#employeeBonusList' );
+  let el = $("#employeeBonusList");
   el.empty();
-  for( let i = 0; i < employees.length; i++) {
+  for (let i = 0; i < employees.length; i++) {
     let employee = bonusCalculator(employees[i]);
-    el.append(`<li id="list"> Name: ${employee.name}<br>Bonus Percentage: ${employee.bonusPercentage}%<br>Total Compensation: ${employee.totalCompensation}<br>Total Bonus: ${employee.totalBonus}</li>`)
+    el.append(
+
+      `<tr>
+          <td>${i + 1}</td>
+          <td>${employee.name}</td>
+          <td>${employee.bonusPercentage}%</td>
+          <td>$${employee.totalCompensation}</td>
+          <td>$${employee.totalBonus}</td>
+      </tr>`
+    );
   } // end for loop
-
-}// end renderDom
-
+} // end renderDom
